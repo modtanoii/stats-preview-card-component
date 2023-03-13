62bdbbf320422cc20cfe7730d574a66b4d3efd0e
modtanoii <modnoiii@gmail.com>
2023-03-13 23:03:39 +0700

    commit

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -8,19 +8,21 @@ function State() {
     <Card>
         <div className="main">
             <div className="left">
-                <p className='title'>Get insights that help your business grow.</p>
+                <span className='title'>Get</span>
+                <span className='title2'> insights </span>
+                <span className='title'>that help your business grow.</span>
 
                 <p className='text'>Discover the benefits of data analytics and make better decisions regarding revenue, customer 
                 experience, and overall efficiency.</p>
 
                 <div className="footer">
-                    <div className="value1">
+                    <div className="value">
                         <h2>10k+</h2> <p>companies</p>
                     </div>
-                    <div className="value2">
+                    <div className="value">
                         <h2>314</h2> <p>templates</p>
                     </div>
-                    <div className="value3">
+                    <div className="value">
                         <h2>12m+</h2> <p>queries</p>
                     </div>
                 </div>
